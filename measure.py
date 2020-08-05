d76d07b10d88b4b8c593943c2e21b279b6781d45
Adam Mahameed <adam.spark199@gmail.com>
2020-08-05 16:13:21 +0300

    Final version refinement

--- measure.py
+++ measure.py
@@ -3,12 +3,6 @@ import math
 import numpy
 from skimage.measure._structural_similarity import compare_ssim as ssim
 
-def PSNR(img1, img2):
-    mse = numpy.mean((img1 - img2) ** 2)
-    if mse == 0:
-        return 100
-    PIXEL_MAX = 255.0
-    return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
 
 def get_image_ssim(original_img,noisy_img):
     return ssim(original_img*255.0, noisy_img*255.0,data_range=original_img.max() - noisy_img.min(), multichannel=False)
